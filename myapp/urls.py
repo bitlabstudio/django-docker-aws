040dc98bddc59160cca1e28f5693a8fc073b3b90
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2016-07-06 14:57:35 +0200

    Initial commit

---
+++ myapp/urls.py
@@ -0,0 +1,9 @@
+"""URLs for the ``myapp`` app."""
+from django.conf.urls import patterns, url
+
+from django.views.generic import TemplateView
+
+urlpatterns = patterns(
+    '',
+    url('', TemplateView.as_view(template_name='home.html')),
+)
