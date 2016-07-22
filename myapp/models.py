239fdb83a9605995d76e67e71546c4c6b9f2c08b
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2016-07-22 13:16:46 +0200

    Added test model

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,21 @@
+"""Test models"""
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class TestModel(models.Model):
+    """Just a test, move along"""
+
+    name = models.CharField(
+        verbose_name='Name',
+        max_length=64,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
